dfe9c141d28cb4505b31a0af0e418f2da3645084
Nurettin Şen <nurie487@gmail.com>
2022-04-26 17:38:49 +0300

    Modified

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ function getURL(){
     if(process.env.NODE_ENV === 'production'){
         return 'mongodb://localhost:27017/real_db'
     }else{
-        return 'mongodb://localhost:27017/Blog'
+        return 'mongodb://localhost:27017/test_user'
     }
 }
 
